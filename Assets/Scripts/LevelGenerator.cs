6712fd80c1d15eaf80e935e16df585c3d4547195
Chauchauz <37832512+Chauchauz@users.noreply.github.com>
2020-09-16 12:15:11 +1000

    Level Generator Update
    Instantiates all tiles
    Still requires correction of rotations

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform[] spriteArray;
+    //TextAsset levelMap = Resources.Load<TextAsset>("PacMan Level Map");
+    int[,] levelMap =
+     {
+         {1,2,2,2,2,2,2,2,2,2,2,2,2,7},     // 0: Empty
+         {2,5,5,5,5,5,5,5,5,5,5,5,5,4},     // 1: Outside Corner
+         {2,5,3,4,4,3,5,3,4,4,4,3,5,4},     // 2: Outside Wall
+         {2,6,4,0,0,4,5,4,0,0,0,4,5,4},     // 3: Inside Corner
+         {2,5,3,4,4,3,5,3,4,4,4,3,5,3},     // 4: Inside Wall
+         {2,5,5,5,5,5,5,5,5,5,5,5,5,5},     // 5: Standard Pellet
+         {2,5,3,4,4,3,5,3,3,5,3,4,4,4},     // 6: Power Pellet
+         {2,5,3,4,4,3,5,4,4,5,3,4,4,3},     // 7: T-Junction
+         {2,5,5,5,5,5,5,4,4,5,5,5,5,4},
+         {1,2,2,2,2,1,5,4,3,4,4,3,0,4},
+         {0,0,0,0,0,2,5,4,3,4,4,3,0,3},
+         {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
+         {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
+         {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
+         {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
+     };
+
     void Start()
+    {
+        LoadLevel();
+        CorrectRotations();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadSprite(Transform sprite, Vector3 position) //Instantiates given object at given position with rotation (90, 0, 0) so it faces the camera
     {
-        
+        Instantiate(sprite, position, Quaternion.Euler(new Vector3(90, 0, 0)));
+    }
+
+    public void LoadLevel() //Instantiates all sprites in correct x,z coordinates *16x16 pixels are scaled by 6.25 (100/16) to fit the grid in prefabs already
+    {
+        for (int i = 0; i < levelMap.GetLength(0); i++)
+        {
+            for (int j = 0; j < levelMap.GetLength(1); j++)
+            {
+                LoadSprite(spriteArray[levelMap[i, j]], new Vector3(j, 0, -i));
+            }
+        }
+    }
+
+    public void CorrectRotations() //Rotates all walls and corner pieces correctly based on surrounding walls
+    {
+        for (int i = 0; i < levelMap.GetLength(0); i++)
+        {
+            for (int j = 0; j < levelMap.GetLength(1); j++)
+            {
+                switch (levelMap[i, j])
+                {
+                    case 
+                }
+            }
+        }
     }
 }
